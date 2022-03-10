3fe02a7ed06fbb4200a735bd5badd96dd9c80ecb
aionescu01 <alexandru.ionescu5@s.unibuc.ro>
2022-03-10 17:22:36 +0200

    Update

---
+++ ProjectV1/ProjectV1.DAL/Entities/StaffMember.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectV1.DAL.Entities
+{
+    public class StaffMember
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Role { get; set; }
+        [Required]
+        public DateTime Birth_Date { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone_Number { get; set; }
+    }
+}
---
+++ ProjectV1/ProjectV1.DAL/Models/PlayerPostModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectV1.DAL.Models
+{
+    public class PlayerPostModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nationality { get; set; }
+        public DateTime Birth_Date { get; set; }
+        public float Height { get; set; }
+        public string Foot { get; set; }
+        public string Position { get; set; }
+    }
+}
