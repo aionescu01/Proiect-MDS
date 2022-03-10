2c728524164f57698f1d3bd0d41389c9c867e0d6
yanastany <anastan0205@gmail.com>
2022-03-10 17:43:27 +0200

    Componente
    Am facut componenta navigatie

--- frontend/proiect/src/index.js
+++ frontend/proiect/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
